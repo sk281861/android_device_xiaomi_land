512c9011230648877f2bf36f8fc4008b75683f7c
nichcream <c.potato23@gmail.com>
2017-11-05 04:16:22 +0800

    land: Define BOARD_USES_ADRENO for some modules to compile

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -157,6 +157,7 @@ endif
 WITH_DEXPREOPT_BOOT_IMG_ONLY ?= true
 
 #Display
+BOARD_USES_ADRENO                := true
 MAX_EGL_CACHE_KEY_SIZE           := 12*1024
 MAX_EGL_CACHE_SIZE               := 2048*1024
 NUM_FRAMEBUFFER_SURFACE_BUFFERS  := 3
